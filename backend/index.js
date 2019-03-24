b5889820182b501663bf97a68f218448b27cf2f8
Tien Pham <r3dsky02@yahoo.com.vn>
2019-03-24 17:11:20 +0200

    React.js Frontend with Hooks and Context

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 
 import express from 'express';
+import cors from 'cors';
+
 import { getInstagramCount, getTwitterCount } from './lib/scraper';
 import db from './lib/db';
 import './lib/cron';
 
 const app = express();
+app.use(cors());
 
 app.get('/scrape', async (req, res, next) => {
   console.log('Scraping!!');
@@ -15,6 +18,13 @@ app.get('/scrape', async (req, res, next) => {
   res.json({ iCount, tCount });
 });
 
+app.get('/data', async (req, res, next) => {
+  // get the scrape data
+  const twitter = db.value();
+  // respond with json
+  res.json(twitter);
+});
+
 app.listen(2093, () => {
-  console.log(`Example App running on port 2093`);
+  console.log(`Example App running on port http://localhost:2093`);
 });
